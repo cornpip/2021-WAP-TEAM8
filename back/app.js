85dd1d0229b6f74f9ae6bcae13246262903dc517
cornpip <chdnjf1375@naver.com>
2021-05-02 15:56:16 +0900

    db test2

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const db = require('../db/db.js')
+const db = require('./db/db.js')
 const port = 4000
 
 app.use(express.static('../client'))
