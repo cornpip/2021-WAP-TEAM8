c798cc7fef48381ba3d9035122e8063e618bd2ae
cornpip <chdnjf1375@naver.com>
2021-05-27 20:00:15 +0900

    행정구역db, 출력

--- server/app.js
+++ server/app.js
@@ -7,8 +7,21 @@ const db = require('./db/db.js')
 const PORT = 4000
 
 db.connect();
-db.query('select * from insertproduct',function(err,result){
+db.query('select * from locate where 시도="부산광역시" AND 시군구="남구" limit 4000',function(err,result){
     if(err) throw err;
+    console.log(result.length);
+    let locate = new Array();
+    let len = result.length;
+    for(i=0; i<len; i++){
+        locate[i]=result[i].읍면동
+    }
+    let set = new Set(locate);
+    let unilocate = [...set];
+    //console.log(unilocate);
+    let point = unilocate.indexOf(null);
+    //console.log(point)
+    unilocate.splice(point,1);
+    //console.log(unilocate);
     console.log('connect success');
 })
 
@@ -110,6 +123,50 @@ app.post('/oproduct',function(req,res){
     db.query(sql,function(err, result){
         res.send(result);
     })
+})
+
+function locatearr(first, second="undefined", res){
+    //console.log(first);
+    //console.log(second);
+    let sql = `select * from locate where 시도="${first}" limit 4000`
+    let inlist = "시군구"
+    if(second){
+        sql = `select * from locate where 시도="${first}" AND 시군구="${second}" limit 4000`
+        inlist = "읍면동"
+    }
+    db.query(sql, function(err,result){
+        //console.log('hi');
+        //console.log(result);
+        //console.log(inlist)
+        let locate = new Array();
+        let len = result.length
+        for(i=0, inlist=inlist; i<len; i++){
+            if(inlist == "시군구"){
+                locate[i]=result[i].시군구
+            }
+            if(inlist == "읍면동"){
+                locate[i]=result[i].읍면동
+            }
+        }
+        //console.log(locate)
+        let set = new Set(locate);
+        let unilocate = [...set];
+        //console.log(unilocate)
+        let point = unilocate.indexOf(null);
+        //console.log(point)
+        unilocate.splice(point,1);
+        console.log(unilocate);
+        res.send(unilocate);
+    })
+}
+
+app.post('/olocate', function(req,res){
+    let body =req.body;
+    locatearr(body.first, body.second, res)
+})
+
+app.get('/ttt', function(req,res){
+    res.render('test.html')
 })
 
 function makeimage(i, result){
